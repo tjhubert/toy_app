cb63139fbb09e234c9345ea4e9a0bb6e370680c9
tjhubert <tjhubert@live.com>
2015-06-10 20:25:33 -0700

    password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
 	has_many :microposts
+
+	before_save { self.email = self.email.downcase }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  	validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: {case_sensitive: false}
 	validates_presence_of :name
-	validates_presence_of :email
+	has_secure_password
+	# validates :password, presence: true, length: { minimum: 6 }
 end
